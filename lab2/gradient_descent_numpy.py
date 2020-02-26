553f92691cfdccc1713fc965c143991e2081f94a
Viktor Claesson <claesson.viktor@gmail.com>
2020-02-26 15:01:07 +0100

    mums

--- lab2/gradient_descent_numpy.py
+++ lab2/gradient_descent_numpy.py
@@ -1,10 +1,7 @@
-"""Gradient descent for linear regression
+"""Gradient descent for linear regression with numpy
 """
-
-__author__ = 'Pierre Nugues'
-
 import random
-import vector
+import numpy as np
 import datasets
 import matplotlib.pyplot as plt
 
@@ -19,13 +16,16 @@ def sse(X, y, w):
     :param w:
     :return:
     """
-    error = vector.sub(y, vector.mul_mat_vec(X, w))
-    return vector.dot(error, error)
+    error = y - X @ w
+    return error.T @ error
 
 
-def normalize(observations):
-    maxima = [max([obs[i] for obs in observations]) for i in range(len(observations[0]))]
-    return ([[obs[i] / maxima[i] for i in range(len(observations[0]))] for obs in observations], maxima)
+def normalize(Xy):
+    maxima = np.amax(Xy, axis=0)
+    D = np.diag(maxima)
+    D_inv = np.linalg.inv(D)
+    Xy = Xy @ D_inv
+    return (Xy, maxima)
 
 
 def stoch_descent(X, y, alpha, w):
@@ -42,18 +42,18 @@ def stoch_descent(X, y, alpha, w):
     logs_stoch = []
     random.seed(0)
     idx = list(range(len(X)))
-    for epoch in range(1000):
+    for epoch in range(500):
         random.shuffle(idx)
         w_old = w
         for i in idx:
-            loss = y[i] - vector.dot(X[i], w)
-            gradient = vector.mul(loss, X[i])
-            w = vector.add(w, vector.mul(alpha, gradient))
+            loss = y[i] - X[i] @ w
+            gradient = loss * X[i].reshape(-1, 1)
+            w = w + alpha * gradient
             logs_stoch += (w, alpha, sse(X, y, w))
-        if vector.norm(vector.sub(w, w_old)) / vector.norm(w) < 1.0e-5:
+        if np.linalg.norm(w - w_old) / np.linalg.norm(w) < 0.005:
+            print("Epoch", epoch)
             break
         logs += (w, alpha, sse(X, y, w))
-    print("Epoch", epoch)
     return w
 
 
@@ -69,40 +69,44 @@ def batch_descent(X, y, alpha, w):
     global logs
     logs = []
     alpha /= len(X)
-    for epoch in range(1, 1000):
-        loss = vector.sub(y, vector.mul_mat_vec(X, w))
-        gradient = vector.mul_mat_vec(vector.transpose(X), loss)
+    for epoch in range(1, 500):
+        loss = y - X @ w
+        gradient = X.T @ loss
         w_old = w
-        w = vector.add(w, vector.mul(alpha, gradient))
+        w = w + alpha * gradient
         logs += (w, alpha, sse(X, y, w))
-        if vector.norm(vector.sub(w, w_old)) / vector.norm(w) < 1.0e-5:
+        if np.linalg.norm(w - w_old) / np.linalg.norm(w) < 0.0005:
+            print("Epoch", epoch)
             break
-    print("Epoch", epoch)
     return w
 
 
 if __name__ == '__main__':
     normalized = True
     debug = False
-    X, y = datasets.read_tsv(
-        'resources/salammbo_a_en.tsv')
+    X, y = datasets.load_tsv(
+        'https://raw.githubusercontent.com/pnugues/ilppp/master/programs/ch04/salammbo/salammbo_a_en.tsv')
+    # Predictors
+    X = np.array(X)
+    # Response
+    y = np.array([y]).T
 
     alpha = 1.0e-10
     if normalized:
         X, maxima_X = normalize(X)
-        maxima_y = max(y)
-        y = [yi / maxima_y for yi in y]
-        maxima = maxima_X + [maxima_y]
+        y, maxima_y = normalize(y)
+        maxima = np.concatenate((maxima_X, maxima_y))
         alpha = 1.0
         print("-Normalized-")
 
     print("===Batch descent===")
-    w = [0.0] * (len(X))
+    w = np.zeros(X.shape[1]).reshape((-1, 1))
     w = batch_descent(X, y, alpha, w)
     print("Weights", w)
     print("SSE", sse(X, y, w))
     if normalized:
-        print("Restored weights", [w[i] * maxima[-1] / maxima[i] for i in range(len(w))])
+        maxima = maxima.reshape(-1, 1)
+        print("Restored weights", maxima[-1, 0] * (w / maxima[:-1, 0:1]))
     if debug:
         print("Logs", logs)
     plt.scatter(range(len(logs[2::3])), logs[2::3], c='b', marker='x')
@@ -115,12 +119,13 @@ if __name__ == '__main__':
     plt.show()
 
     print("===Stochastic descent===")
-    w = [0.0] * (len(X))
+    w = np.zeros(X.shape[1]).reshape((-1, 1))
     w = stoch_descent(X, y, alpha, w)
     print("Weights", w)
     print("SSE", sse(X, y, w))
     if normalized:
-        print("Restored weights", [w[i] * maxima[-1] / maxima[i] for i in range(len(w))])
+        maxima = maxima.reshape(-1, 1)
+        print("Restored weights", maxima[-1, 0] * (w / maxima[:-1, 0:1]))
     if debug:
         print("Logs", logs)
         print("Logs stoch.", logs_stoch)
--- lab2/linear.py
+++ lab2/linear.py
@@ -1,19 +1,37 @@
 import random
 import vector
 import datasets
+import numpy as np
 import matplotlib.pyplot as plt
 
-def normalize(observations):
-    return observations
+def normalize(obs):
+    print(obs)
+    maxima = np.amax(obs, axis=0)
+    print(maxima)
+    D = np.diag(maxima)
+    print(D)
+    D_inv = np.linalg.inv(D)
+    print(D_inv)
+    obs = obs @ D_inv
+    print(obs)
+    return (obs, maxima)
+
 
 def stoch_descent(X, y, alpha, w):
     return w
 
+
 def batch_descent(X, y, alpha, w):
+    alpha /= len(X)
+    
+
+
+
     return w
 
+
 if __name__ == '__main__':
-    normalized = True
-    debug = False
-    X, y = datasets.read_tsv('resources/salammbo_a_en.tsv')
-    print(X, y)
+    X_en, y_en = datasets.read_tsv('resources/salammbo_a_en.tsv')
+    
+    normalize(X_en)
+    #w = batch_descent(X_en, y_en, 0.001, w)
