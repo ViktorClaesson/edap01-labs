c92285e111fb7b58456dd555c21690894c6e6480
Viktor Claesson <claesson.viktor@gmail.com>
2020-02-26 22:21:20 +0100

    more changes

--- lab2/logistic_regression.py
+++ lab2/logistic_regression.py
@@ -6,12 +6,12 @@ import matplotlib.pyplot as plt
 def threshold(w, x):
     return 1 / (1 + np.exp(-w @ x))
 
-def logistic_regression(X, y, w, idx, alpha=1.0):
-    # idx = list(range(len(X)))
+def logistic_regression(X, y, w, alpha=1.0):
+    index_list = list(range(len(X)))
     for epoch in range(500):
-        random.shuffle(idx)
+        random.shuffle(index_list)
         w_old = w
-        for i in idx:
+        for i in index_list:
             w = w + alpha * X[i] * (y[i] - threshold(w, X[i]))
         if np.linalg.norm(w - w_old) / np.linalg.norm(w) < 0.005:
             print('Epoch: {}'.format(epoch))
@@ -19,18 +19,18 @@ def logistic_regression(X, y, w, idx, alpha=1.0):
     return w
 
 def loocv_logreg(X, y, w, alpha=1.0):
-    idx = list(range(len(X)))
+    index_list = list(range(len(X)))
     correct = 0
 
     for i in range(len(X)):
-        random.shuffle(idx)
-        loo_idx = idx.pop(0)
+        random.shuffle(index_list)
+        index = index_list.pop(0)
         w = np.zeros(len(X[0]))
-        w = logistic_regression(X, y, w, idx)
-        hw = 1.0 if (threshold(w, X[loo_idx]) >= 0.5) else 0.0
-        if hw == y[loo_idx]:
+        w = logistic_regression(X, y, w, index)
+        hw = 1.0 if (threshold(w, X[index]) >= 0.5) else 0.0
+        if hw == y[index]:
             correct += 1
-        idx.append(loo_idx)
+        index_list.append(index)
     print('Correct: {}'.format(correct))
     return w
 
@@ -62,7 +62,7 @@ if __name__ == "__main__":
 
     alpha = 1.0
 
-    w = logistic_regression(X, y, w, list(range(len(X))))
+    w = logistic_regression(X, y, w)
     print(w)
     plot_log(X[:15], X[15:], w)
 
